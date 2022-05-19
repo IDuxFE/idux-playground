34e12ae133f8dcaefaaa6b8d4fce4cf1dab0c8f6
X.Q. Chen <31237954+brenner8023@users.noreply.github.com>
2022-05-19 23:57:27 +0800

    fix: 修复组件图标显示不出问题

--- src/template/App.vue
+++ src/template/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div style="padding: 20px 100px;">
-    <p>猜谜语：</p>
-    <p>我在上，你在下，你不动，我不动。</p>
-    <p>我一动你就痛，你出水我高兴。</p>
-    <IxDivider />
-    打一活动。
-    <IxSwitch :labels="['钓鱼', '查看谜底']"></IxSwitch>
+    <IxTransfer :data-source="[]" />
   </div>
 </template>
 
--- src/template/setupIdux.js
+++ src/template/setupIdux.js
@@ -1,6 +1,12 @@
 import { getCurrentInstance } from 'vue'
 import IduxComponents from '@idux/components'
 import IduxCdk from '@idux/cdk'
+import {
+  IDUX_ICON_DEPENDENCIES,
+  addIconDefinitions,
+} from '@idux/components';
+
+addIconDefinitions(IDUX_ICON_DEPENDENCIES);
 
 const install = (app) => {
   app.use(IduxCdk).use(IduxComponents)
